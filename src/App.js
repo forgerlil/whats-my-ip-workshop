1176fc9f954fcb0c98e5436d58b00885d1c92951
Lilian Forger <lilianforger@gmail.com>
2022-04-29 18:11:14 +0200

    To work on the weekend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
-import './App.css';
+import "./App.css";
+import { Map, Marker } from "pigeon-maps";
+import { useState, useEffect } from "react";
+import { getUserInfo } from "./fetches/fetches";
+
+/*
+ Needed data: 
+   User Ip, ***CHECK***
+   Get Geolocation based on IP to pass to anchor in Marker component
+*/
 
 function App() {
+  const [ipInfo, setIpInfo] = useState({});
+
+  // useEffect(() => {
+  //   (async () => {
+  //     setIpInfo(await getUserInfo())
+  //   })();
+
+  // }, [])
+
   return (
-    <div>
-      Start of the whats my ip app!
-    </div>
+    <>
+      <Map height={600} defaultCenter={[50.11552, 8.68417]} defaultZoom={11}>
+        {/* <Marker width={50} anchor={[ipInfo.lat, ipInfo.lng]} /> */}
+        {/* <img className="bg-img" src="" alt="" /> */}
+        <div className="ip-container">
+          <div className="title-wrapper">
+            <img src="" alt="" />
+            <h1>What's my IP?</h1>
+          </div>
+          <img src="" alt="" />
+          <div className="ip-details-wrapper">
+            <p>Your IP address is {ipInfo.ip}</p>
+            <p>
+              You are currently located in {ipInfo.city}, {ipInfo.country}
+            </p>
+          </div>
+          <div>
+            <div className="date-wrapper">
+              <img src="" alt="" />
+              <p>Today is the DD/MM/YYYY</p>
+            </div>
+            <div className="time-wrapper">
+              <img src="" alt="" />
+              <p>Your local time is HH:MM</p>
+            </div>
+          </div>
+        </div>
+      </Map>
+    </>
   );
 }
 
---
+++ src/fetches/fetches.js
@@ -0,0 +1,50 @@
+const getUserInfo = async () => {
+    const response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_API_KEY}`)
+    const json = await response.json();
+    console.log(json)
+    const ip = await json.ip;
+    const country = await json.location.country;
+    const city = await json.location.city;
+    const lat = await json.location.lat;
+    const lng = await json.location.lng;
+    const timezone = await json.location.timezone;
+
+    const userLocation = {
+        ip: ip,
+        country: country,
+        city: city,
+        lat: lat,
+        lng: lng,
+        timezone: timezone
+    }
+
+    return userLocation;
+}
+
+export { getUserInfo }
+
+// {
+//     "ip": "2.204.228.10",
+//     "location": {
+//       "country": "DE",
+//       "region": "Hessen",
+//       "city": "Frankfurt am Main",
+//       "lat": 50.11552,
+//       "lng": 8.68417,
+//       "postalCode": "60559",
+//       "timezone": "+02:00",
+//       "geonameId": 2925533
+//     },
+//     "domains": [
+//       "dslb-002-204-228-010.002.204.pools.vodafone-ip.de",
+//       "ip-2-204-228-10.web.vodafone.de"
+//     ],
+//     "as": {
+//       "asn": 3209,
+//       "name": "VODANET",
+//       "route": "2.204.0.0/14",
+//       "domain": "http://www.vodafone.de",
+//       "type": "Cable/DSL/ISP"
+//     },
+//     "isp": "Vodafone GmbH"
+//   }
