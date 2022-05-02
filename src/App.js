2d229641a73bded96f328bf43c02cd1ce175bd6a
ranmacar <102212010+WBS-MartinV@users.noreply.github.com>
2022-05-02 17:57:55 +0200

    questions

--- src/App.js
+++ src/App.js
@@ -1,68 +1,113 @@
 import "./App.css";
-import { Map, Marker } from 'pigeon-maps';
-import { useState, useEffect } from 'react';
-import { getUserInfo } from './fetches/fetches';
-import locationmarker from './pointer.png'
-import calendarIcon from './calendar.png';
-import { DateTime } from 'luxon';
-
+import { Map, Marker } from "pigeon-maps";
+import { useState, useEffect } from "react";
+import { getUserInfo } from "./fetches/fetches";
+import locationmarker from "./pointer.png";
+import calendarIcon from "./calendar.png";
+import { DateTime } from "luxon";
 
 function App() {
-  const [ipInfo, setIpInfo] = useState({});
+    const [ipInfo, setIpInfo] = useState({});
 
-  const [dateTime, setDateTime] = useState(DateTime.local());
-  
-  useEffect(() => {
-    // Keep this commented out unless there's need to check fetch results, as fetch qty is limited (free api sub)!
-    // (async () => {
-    //   setIpInfo(await getUserInfo())
-    // })();
-    const startInterval = setInterval(() => setDateTime(DateTime.local()), 1000)
+    const [dateTime, setDateTime] = useState(DateTime.local());
 
-  return () => clearInterval(startInterval)
-  }, [])
+    const padWith0 = (number) =>
+        number < 10 ? "0" + number : number.toString();
 
-  
+    useEffect(() => {
+        // Keep this commented out unless there's need to check fetch results, as fetch qty is limited (free api sub)!
+        (async () => {
+            setIpInfo(await getUserInfo());
+        })();
 
-  return (
-    ipInfo &&
-    <div className="root-wrapper">
-      {ipInfo.lat}
-      {/* <Map defaultCenter={[ipInfo.lat, ipInfo.lat]} defaultZoom={11}> -- This isn't working! =( */}
-      <Map defaultCenter={[50, 8]} defaultZoom={11}>
-        <Marker width={50} anchor={[50, 8]} />
-        <div className="IP-card"> 
-          <div className="title-wrapper">
-            <img src={locationmarker} alt="Location marker" className="marker" />
-            <h1 className="IP-title"><strong>What's my IP?</strong></h1>
-          </div>     
-          <div className="ip-container">
-            <img src="https://upload.wikimedia.org/wikipedia/en/b/ba/Flag_of_Germany.svg" alt="" className="flag-img" />
-            <div className="ip-details-wrapper">
-              <h3><strong>Your IP address is {ipInfo.ip}</strong></h3>
-              <p>
-                You are currently located in {ipInfo.city}, {ipInfo.country}
-              </p>
+        console.log("useEffect!"); // this only happens twice, the interval doesn't retrigger a fetch
+
+        const startInterval = setInterval(
+            () => setDateTime(DateTime.local()),
+            1000
+        );
+
+        return () => clearInterval(startInterval);
+    }, []);
+
+    return (
+        ipInfo && (
+            <div className="root-wrapper">
+                <Map center={ipInfo.location} defaultZoom={13}>
+                    <Marker width={50} anchor={ipInfo.location} />
+                </Map>
+                <div className="IP-card">
+                    <div className="title-wrapper">
+                        <img
+                            src={locationmarker}
+                            alt="Location marker"
+                            className="marker"
+                        />
+                        <h1 className="IP-title">
+                            <strong>What's my IP?</strong>
+                        </h1>
+                    </div>
+                    <div className="ip-container">
+                        <img
+                            src="https://upload.wikimedia.org/wikipedia/en/b/ba/Flag_of_Germany.svg"
+                            alt=""
+                            className="flag-img"
+                        />
+                        <div className="ip-details-wrapper">
+                            <h3>
+                                <strong>Your IP address is {ipInfo.ip}</strong>
+                            </h3>
+                            <p>
+                                You are currently located in {ipInfo.city},{" "}
+                                {ipInfo.country}
+                            </p>
+                        </div>
+                        <hr />
+                        <div className="date-time">
+                            <div className="date-wrapper">
+                                <img
+                                    src={calendarIcon}
+                                    alt="Calendar Icon"
+                                    className="calendar-icon"
+                                />
+                                <p>
+                                    Today is the{" "}
+                                    {dateTime.c.day < 10
+                                        ? `0${dateTime.c.day}`
+                                        : dateTime.c.day}
+                                    /
+                                    {dateTime.c.month < 10
+                                        ? `0${dateTime.c.month}`
+                                        : dateTime.c.month}
+                                    /{dateTime.c.year}
+                                </p>
+                            </div>
+                            <div className="time-wrapper">
+                                <img
+                                    src="https://cdn-icons-png.flaticon.com/512/109/109613.png"
+                                    alt="Clock Icon"
+                                    className="clock-icon"
+                                />
+                                <p>
+                                    Your local time is{" "}
+                                    {[
+                                        dateTime.c.hour,
+                                        dateTime.c.minute,
+                                        dateTime.c.second,
+                                    ]
+                                        .map(padWith0)
+                                        .join(":")}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <hr />
-            <div className="date-time">
-              <div className="date-wrapper">
-                <img src={calendarIcon} alt="Calendar Icon" className="calendar-icon" />
-                <p>Today is the {dateTime.c.day < 10 ? `0${dateTime.c.day}` : dateTime.c.day}/{dateTime.c.month < 10 ? `0${dateTime.c.month}` : dateTime.c.month}/{dateTime.c.year}</p>
-              </div>
-              <div className="time-wrapper">
-                <img src="https://cdn-icons-png.flaticon.com/512/109/109613.png" alt="Clock Icon" className="clock-icon" />
-                <p>Your local time is {dateTime.c.hour < 10 ? `0${dateTime.c.hour}` : dateTime.c.hour}:{dateTime.c.minute < 10 ? `0${dateTime.c.minute}` : dateTime.c.minute}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Map>
-    </div>
-  );
+        )
+    );
 }
 
-        export default App;
+export default App;
 
 /*
  Questions!
--- src/fetches/fetches.js
+++ src/fetches/fetches.js
@@ -1,22 +1,42 @@
 const getUserInfo = async () => {
     const response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_API_KEY}`)
     const json = await response.json();
-    // console.log(json)
-    const ip = await json.ip;
-    const country = await json.location.country;
-    const city = await json.location.city;
-    const lat = await json.location.lat;
-    const lng = await json.location.lng;
-    const timezone = await json.location.timezone;
 
+    // const json = {
+    //     "ip": "84.162.165.246",
+    //     "location": {
+    //         "country": "DE",
+    //         "region": "Hessen",
+    //         "city": "Frankfurt am Main",
+    //         "lat": 50.11552,
+    //         "lng": 8.68417,
+    //         "postalCode": "60559",
+    //         "timezone": "+02:00",
+    //         "geonameId": 2925533
+    //     },
+    //     "domains": [
+    //         "pzxzilxbzvswrqcc.myfritz.net"
+    //     ],
+    //     "as": {
+    //         "asn": 3320,
+    //         "name": "DTAG",
+    //         "route": "84.128.0.0/10",
+    //         "domain": "https://globalcarrier.telekom.com",
+    //         "type": "NSP"
+    //     },
+    //     "isp": "Deutsche Telekom AG"
+    // }
+
+    // After you await the json its just a normal value don't have to await each proprty
     const userLocation = {
-        ip: ip,
-        country: country,
-        city: city,
-        lat: lat,
-        lng: lng,
-        timezone: timezone
+        ip: json.ip,
+        country: json.location.country,
+        city: json.location.city,
+        location: [json.location.lat, json.location.lng],
+        timezone: json.location.timezone
     }
+
+    // console.log(userLocation)
 
     return userLocation;
 }
